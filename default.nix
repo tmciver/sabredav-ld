0d3c9ac6fd08af6cb4096f5cab1c8e7d2082afff
Tim McIver <tim@timmciver.com>
2023-01-24 22:04:11 -0500

    Don't return an attribute set . . .
    return the derivation directly.

--- default.nix
+++ default.nix
@@ -1,17 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
 
 with pkgs;
-let
-  pwd = builtins.toString ./.;
-in
-{
-  source-code = stdenv.mkDerivation {
-    name = "sabredav-ld";
-    src = ./.;
-    phases = [ "installPhase" ];
-    installPhase = ''
-      mkdir $out
-      cp $src/index.php $out/
-    '';
-  };
+stdenv.mkDerivation {
+  name = "sabredav-ld";
+  src = ./.;
+  phases = [ "installPhase" ];
+  installPhase = ''
+    mkdir $out
+    cp $src/index.php $out/
+  '';
 }
